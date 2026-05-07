a431f3dd7301f0ca01a47c30afb2384947c9d618
madebyrsh <madebyrsh.dev@gmail.com>
2026-05-08 00:20:28 +0900

    Complete Chapter2

--- SwiftUI/ViewsStructuresAndProperties/WeatherForecast/WeatherForecast/ContentView.swift
+++ SwiftUI/ViewsStructuresAndProperties/WeatherForecast/WeatherForecast/ContentView.swift
@@ -11,22 +11,53 @@ struct ContentView: View {
     var body: some View {
         HStack {
             
-            DayForecast() // DayForecast Struct를 SubView로 표시
+            DayForecast(day: "Mon", isRainy: false, high: 70, low: 50) // DayForecast Struct를 SubView로 표시
             
-            DayForecast()
+            DayForecast(day: "Tue", isRainy: true, high: 60, low: 30)
         }
         
     }
 }
 
 struct DayForecast: View {
+    
+    let day: String
+    let isRainy: Bool
+    let high: Int
+    let low: Int
+    
+    var iconName : String {
+        if isRainy {
+            return "cloud.rain.fill"
+        }
+        else {
+            return "sun.max.fill"
+        }
+        
+    }
+    
+    var iconColor : Color {
+        if isRainy {
+            return Color.blue
+        }
+        else {
+            return Color.yellow
+        }
+    }
+    
     var body: some View {
         VStack {
-            Text("Mon")
-            Image(systemName: "sun.max.fill")
-                .foregroundColor(Color.yellow)
-            Text("High: 70")
-            Text("Low: 50")
+            Text(day)
+                .font(Font.headline)
+            Image(systemName: iconName)
+                .foregroundColor(iconColor)
+                .font(Font.largeTitle)
+                .padding(5)
+            Text("High: \(high)º")
+                .fontWeight(Font.Weight.semibold)
+            Text("Low: \(low)º")
+                .fontWeight(Font.Weight.medium)
+                .foregroundColor(Color.secondary)
         }
         .padding()
     }
